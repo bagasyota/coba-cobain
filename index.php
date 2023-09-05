47120e37124424ff793c14396ad9a1c637dbf085
bagas <bagasyota7760@gmail.com>
2023-09-05 11:46:52 +0700

    menambahkan file index.php

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>temen gweh</h1>
+    <ul>
+        <li>jagi :the moon</li>
+        <li>radit :the sun</li>
+        <li>aziz :the eclipse</li>
+    </ul>
+</body>
+</html>
+</body>
+</html>
